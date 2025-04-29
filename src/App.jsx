de345b264548a1080fa79d9b18b1d575007cb543
Preethipriya12345 <lakshmipriya.kali@slu.edu>
2025-04-29 21:01:16 +0000

    chore: fix typos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import Header from './components/Header';
+
 import productData from './data/full-products';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello There.</h1>
+      <Header />
+      <CardList data={productData} />
+    </div>
+  );
+}
+
+import { Route, Routes} from 'react-router-dom';
+
+// ...
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      
+        <Routes>
+          <Route path="/" element={<CardList data={productData} />} />
+          <Route path="/product/:id" element={<SingleView data={productData} />} />
+        </Routes>
+      
     </div>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,3 +7,20 @@ export default function Button({text, handleClick}) {
     </a>
   )
 }
+return (
+  <div className="cf pa2">
+
+    <div className="mt2 mb2">
+      // Using the data prop, we map over the list of products and render a Card component for each product
+      {data.map((product) => (
+        <Card key={product.id} {...product} />
+      ))}
+    </div>
+    
+    //  Pagination Buttons
+    <div className="flex items-center justify-center pa4">   
+      <Button text="Previous" />
+      <Button text="Next" />
+    </div>
+  </div>
+)
